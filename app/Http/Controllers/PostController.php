392f70ea3a1287eaf2d6e41e661e1f8e5b3f1e4a
Dani Valero <danivalero98@hotmail.com>
2023-06-06 11:07:11 +0200

    First commit, boilerplate

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+
+
+class PostController {
+    public function index() {
+        $posts = Post::get();
+        return view("blog", ["posts" => $posts]);
+    }
+}
